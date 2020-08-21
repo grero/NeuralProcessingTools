1447194af76e0e135f8480ae3dc0364333bdcf18
roger <roger.herikstad@gmail.com>
2020-08-21 16:56:33 +0800

    Bumps version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="NeuralProcessingTools",
-      version="0.1.0",
+      version="0.2.0",
       description="""Tools for processing neural data""",
       url="https://github.com/grero/NeuralProcessingTools.git",
       author="Roger Herikstad",
