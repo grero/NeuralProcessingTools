bf713b4e6df86dcf1fa7942480e1936f3e84c408
Roger Herikstad <roger.herikstad@gmail.com>
2021-01-06 12:17:03 +0800

    Oldtrials (#12)
    * Refactoring and code for handling old trials
    
    * Fixes an issue with old trials
    
    * Fixes issues with create_events
    
    * Removes debug output
    
    * Adds tests for old trial structure
    
    * Bumps version number
    
    * Updates tests for old trials to download testdata
    
    * Fixes an issue with parsing stimulus from old trial data
    
    * Adds more tests
    
    * Refactoring to parent class
    
    * Use linear index rather than cartesian index for old trials
    
    * Fixes an issue with removing test data
    
    * Adds tests for stimulus locations

---
+++ tests/test_oldtrials.py
@@ -0,0 +1,33 @@
+import DataProcessingTools as DPT
+import NeuralProcessingTools as NPT
+from NeuralProcessingTools.trialstructures import OldWorkingMemoryTrials
+import numpy as np
+import tempfile
+import wget
+import os
+
+
+def test_oldtrials():
+    tempdir = tempfile.gettempdir()
+    with DPT.misc.CWD(tempdir):
+        pth = "Pancake/20130923/session01"
+        if not os.path.isdir(pth):
+            os.makedirs(pth)
+        
+        with DPT.misc.CWD(pth):
+            do_unlink = False
+            if not os.path.isfile("event_data.mat"):
+                wget.download("http://cortex.nus.edu.sg/testdata/J20140807_event_data.mat", "event_data.mat")
+                do_unlink = True
+            trials = OldWorkingMemoryTrials()
+            assert (trials.events == "trial_start").sum() == 934
+            assert (trials.events == "reward_on").sum() == 369
+            print(trials.events)
+            tidx = np.where(trials.events == "stimulus_on_1_6")[0]
+            assert tidx[0] == 2
+            didx = np.where(trials.events == "stimulus_on_2_14")[0]
+            assert didx[0] == 323
+            assert trials.ncols == 4
+            assert trials.nrows == 4
+            if do_unlink:
+                os.unlink("event_data.mat")
